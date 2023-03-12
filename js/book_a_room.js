e819dcf08ebaa4a8fcbeae2b3d9699e87c7a52ba
HseinKt <hseinkteish1998@gmail.com>
2023-03-13 00:43:57 +0200

    added book a room JS page

---
+++ js/book_a_room.js
@@ -0,0 +1,28 @@
+
+const submit_button = document.getElementById("submit_button")
+
+submit_button.addEventListener('click', () => {
+    const patient = document.getElementById("patient").value
+    const department = document.getElementById("department").value
+    const room = document.getElementById("room").value
+    const bed = document.getElementById("bed").value
+
+    const data = new FormData();
+    data.append('user_id',patient);
+    data.append('department_id',department);
+    data.append('room_id',room);
+    data.append('bed_id',bed);
+
+    axios
+        .post('http://localhost/healthnet/healthnet-backend/book-a-room.php',data)
+        .then((result) => {
+            if (result.status == 200){
+                console.log("acess 1");
+                console.log(result.data.id);
+            }
+        })
+        .catch((err) => {
+                console.error('Error in outer API call:', err);
+        });
+})
+
