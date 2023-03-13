17fcb1b481a1452ad52c1a342f452e6242f1c8b7
HseinKt <hseinkteish1998@gmail.com>
2023-03-13 03:54:41 +0200

    updated services page

--- js/services.js
+++ js/services.js
@@ -8,7 +8,6 @@ submit_button.addEventListener('click', () => {
     const cost = document.getElementById("cost").value
     const description = document.getElementById("description").value
     
-
     const data = new FormData();
     data.append('patient_id',patient);
     data.append('employee_id',employee);
@@ -20,9 +19,24 @@ submit_button.addEventListener('click', () => {
         .post('http://localhost/healthnet/healthnet-backend/request_services.php',data)
         .then((result) => {
             if (result.status == 200){
-                console.log("acess 1");
-                console.log(result.data.id);
-                window.location.href = "../Index.html"
+                console.log("request success ");
+                const confirmation = confirm(" asking the admin to approve ")
+                if(confirmation) {
+
+                    const data2 = new FormData();
+                    data2.append('user_id',patient);
+                    axios
+                    .post('http://localhost/healthnet/healthnet-backend/admin_approve.php',data2)
+                    .then((result) => {
+                        if (result.status == 200){
+                            console.log("approve success ");
+                            window.location.href = "../Index.html";
+                        }
+                    })
+                    .catch((err) => {
+                            console.error('Error in outer API call:', err);
+                    });                    
+                }
             }
         })
         .catch((err) => {
